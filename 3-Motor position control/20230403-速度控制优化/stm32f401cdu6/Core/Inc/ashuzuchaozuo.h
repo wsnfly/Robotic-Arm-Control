db920aa38f922807d6dcd42684b129f13992424d
wsnfly <441434517@qq.com>
2023-04-03 21:24:31 +0800

    速度控制优化

---
+++ 3-Motor position control/20230403-速度控制优化/stm32f401cdu6/Core/Inc/ashuzuchaozuo.h
@@ -0,0 +1,16 @@
+/*
+ * ashuzuchaozuo.h
+ *
+ *  Created on: 2020年5月14日
+ *      Author: Administrator
+ */
+
+#ifndef INC_ASHUZUCHAOZUO_H_
+#define INC_ASHUZUCHAOZUO_H_
+
+int32_t xunzhaoshuzuuint8_t(uint8_t *,int32_t ,uint8_t *,int32_t ,int32_t );
+
+uint8_t* shuzidaowenben2(int64_t,uint32_t*,uint8_t*);
+int32_t wenbendaoshuzi(uint8_t* ,uint8_t );
+
+#endif /* INC_ASHUZUCHAOZUO_H_ */
---
+++ 3-Motor position control/20230403-速度控制优化/串口.py
@@ -0,0 +1,55 @@
+import serial
+import time
+
+# 配置串口
+ser = serial.Serial()
+ser.port = 'COM9'  # 串口号
+ser.baudrate = 115200  # 波特率
+ser.bytesize = 8
+ser.stopbits = 1
+ser.parity = 'N'
+
+
+def calc_crc16_modbus(data):
+    crc = 0xFFFF
+    for byte in data:
+        crc ^= byte
+        for i in range(8):
+            if crc & 0x0001:
+                crc = (crc >> 1) ^ 0xA001
+            else:
+                crc = crc >> 1
+    return crc
+
+
+# 打开串口
+ser.open()
+
+# 数组数据
+t = 10000
+data = [0xcc,0x01,0x06,0x00,0x04,0x00,0x00,0x00,0x00,0x00,0x00,0x00,100]
+weizhi = 0
+
+
+# 将数组转换为字节数组
+# byte_data = bytearray(data)
+cr = calc_crc16_modbus(data)
+crc_high_byte = (cr >> 8) & 0xFF
+crc_low_byte = cr & 0xFF
+data.append(crc_low_byte)
+data.append(crc_high_byte)
+print(data)
+print(1)
+# 发送数据到串口
+
+ser.write(data)
+time.sleep(0.1)
+dat = []
+dat = ser.read_all()  # 读取一行数据，以换行符为结束符
+print(2)
+print(dat)  # 
+for i in dat:
+    print(i)
+
+# 关闭串口
+ser.close()
